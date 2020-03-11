24a5c2cc496416531f3526cd921f276fbac19077
jinchangxfd <1660857877@qq.com>
2020-03-11 23:41:53 +0800

    Brand judgement and unit test 2

--- Texas_Holdem.py
+++ Texas_Holdem.py
@@ -1,6 +1,82 @@
+from functools import cmp_to_key
+
+
+# 判断大小 计算差值
+def cmp(a, b):
+    lt_list = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+    return lt_list.index(a[0]) - lt_list.index(b[0])
+
+
 # 判断牌型
-def poker_hand(l=[]):
-    return 0
+def poker_hand(hand_list=[]):
+    re_list = sorted(hand_list, key=cmp_to_key(cmp))
+    print(re_list)
+
+    # 同花顺
+    if re_list[0][1] == re_list[1][1] == re_list[2][1] == re_list[3][1] == re_list[4][1] \
+            and cmp(re_list[0], re_list[1]) == cmp(re_list[1], re_list[2]) == cmp(re_list[2], re_list[3]) == cmp(
+                re_list[3], re_list[4]) == -1:
+        return ['Straight flush', re_list[4][0]]
+
+    # 铁支
+    if re_list[0][0] == re_list[1][0] == re_list[2][0] == re_list[3][0]:
+        return ['Four of a kind', re_list[0][0]]
+    if re_list[4][0] == re_list[1][0] == re_list[2][0] == re_list[3][0]:
+        return ['Four of a kind', re_list[4][0]]
+
+    # 葫芦
+    if re_list[0][0] == re_list[1][0] == re_list[2][0] and re_list[3][0] == re_list[4][0]:
+        return ['Full House', re_list[0][0]]
+    if re_list[0][0] == re_list[1][0] and re_list[3][0] == re_list[4][0] == re_list[2][0]:
+        return ['Full House', re_list[4][0]]
+
+    # 同花
+    if re_list[0][1] == re_list[1][1] == re_list[2][1] == re_list[3][1] == re_list[4][1]:
+        return ['Flush', re_list[4][0], re_list[3][0], re_list[2][0], re_list[1][0], re_list[0][0]]
+
+    # 顺子
+    if cmp(re_list[0], re_list[1]) == cmp(re_list[1], re_list[2]) == cmp(re_list[2], re_list[3]) == cmp(
+            re_list[3], re_list[4]) == -1:
+        return ['Straight', re_list[4][0]]
+
+    # 三条
+    if re_list[0][0] == re_list[1][0] == re_list[2][0]:
+        return ['Three of a Kind', re_list[0][0]]
+    if re_list[3][0] == re_list[1][0] == re_list[2][0]:
+        return ['Three of a Kind', re_list[3][0]]
+    if re_list[4][0] == re_list[3][0] == re_list[2][0]:
+        return ['Three of a Kind', re_list[4][0]]
+
+    # 两对
+    if re_list[0][0] == re_list[1][0] and re_list[2][0] == re_list[3][0]:
+        return ['Two Pairs', re_list[2][0], re_list[0][0], re_list[4][0]]
+    if re_list[0][0] == re_list[1][0] and re_list[4][0] == re_list[3][0]:
+        return ['Two Pairs', re_list[4][0], re_list[0][0], re_list[2][0]]
+    if re_list[2][0] == re_list[1][0] and re_list[4][0] == re_list[3][0]:
+        return ['Two Pairs', re_list[4][0], re_list[2][0], re_list[0][0]]
+
+    # 对子
+    if re_list[0][0] == re_list[1][0]:
+        return ['Pair', re_list[0][0], re_list[4][0], re_list[3][0], re_list[2][0]]
+    if re_list[2][0] == re_list[1][0]:
+        return ['Pair', re_list[2][0], re_list[4][0], re_list[3][0], re_list[0][0]]
+    if re_list[2][0] == re_list[3][0]:
+        return ['Pair', re_list[2][0], re_list[4][0], re_list[1][0], re_list[0][0]]
+    if re_list[3][0] == re_list[4][0]:
+        return ['Pair', re_list[3][0], re_list[2][0], re_list[1][0], re_list[0][0]]
+
+    # 散牌
+    return ['High Card', re_list[4][0], re_list[3][0], re_list[2][0], re_list[1][0], re_list[0][0]]
+
+
+# 按牌型比大小
+def cmp_poker(a, b):
+    lt_list = ['High Card', 'Pair', 'Two Pairs', 'Three of a Kind', 'Straight', 'Flush', 'Full House',
+               'Four of a kind', 'Straight flush']
+    if lt_list.index(a[0]) > lt_list.index(b[0]):
+        return 1
+    if lt_list.index(a[0]) > lt_list.index(b[0]):
+        return 0
 
 
 # 判断胜负
@@ -14,6 +90,7 @@ def who_win(input_str):
     for i in range(1, 6):
         hand1.append(list(input_list[i]))
     print(hand1)
+    poker_hand(hand1)
     name2 = input_list[6][:-1]
     for i in range(7, 12):
         hand2.append(list(input_list[i]))
@@ -21,4 +98,4 @@ def who_win(input_str):
 
 
 if __name__ == '__main__':
-    who_win('Black: 6H 7D 7S 7H 7C White: 2C 3H 4S 8C AH')
+    who_win('Black: AH KD 7S 4H 7C White: 2C 3H 4S 8C AH')
--- unit_test.py
+++ unit_test.py
@@ -1,17 +1,32 @@
 import unittest
-from Texas_Holdem import poker_hand
 
-class TestStringMethods(unittest.TestCase):
+from Texas_Holdem import poker_hand,who_win
+
+
+class TestMethods(unittest.TestCase):
     def test_poker_hand(self):
-        self.assertIs('High Card',poker_hand([['2', 'H'], ['7', 'D'], ['J', 'S'], ['9', 'H'], ['T', 'H']]))
-        self.assertIs('Pair',poker_hand([['7', 'H'], ['7', 'D'], ['J', 'S'], ['9', 'H'], ['T', 'H']]))
-        self.assertIs('Two Pairs',poker_hand([['7', 'H'], ['7', 'D'], ['9', 'S'], ['9', 'H'], ['T', 'H']]))
-        self.assertIs('Three of a Kind',poker_hand([['7', 'H'], ['7', 'D'], ['7', 'S'], ['9', 'H'], ['T', 'H']]))
-        self.assertIs('Straight',poker_hand([['6', 'H'], ['7', 'D'], ['8', 'H'], ['9', 'H'], ['T', 'H']]))
-        self.assertIs('Flush',poker_hand([['6', 'H'], ['7', 'H'], ['8', 'H'], ['T', 'H'], ['J', 'H']]))
-        self.assertIs('Full House',poker_hand([['6', 'H'], ['6', 'D'], ['7', 'S'], ['7', 'H'], ['7', 'C']]))
-        self.assertIs('Four of a kind',poker_hand([['6', 'H'], ['7', 'D'], ['7', 'S'], ['7', 'H'], ['7', 'C']]))
-        self.assertIs('Straight flush',poker_hand([['6', 'H'], ['7', 'H'], ['8', 'H'], ['9', 'H'], ['T', 'H']]))
+        self.assertEqual(['High Card', 'J', 'T', '9', '7', '2'],
+                         poker_hand([['2', 'H'], ['7', 'D'], ['J', 'S'], ['9', 'H'], ['T', 'H']]))
+        self.assertEqual(['Pair', '7', 'J', 'T', '9'],
+                         poker_hand([['7', 'H'], ['7', 'D'], ['J', 'S'], ['9', 'H'], ['T', 'H']]))
+        self.assertEqual(['Two Pairs', '9', '7', 'T'],
+                         poker_hand([['7', 'H'], ['7', 'D'], ['9', 'S'], ['9', 'H'], ['T', 'H']]))
+        self.assertEqual(['Three of a Kind', '7'],
+                         poker_hand([['7', 'H'], ['7', 'D'], ['7', 'S'], ['9', 'H'], ['T', 'H']]))
+        self.assertEqual(['Straight', 'T'], poker_hand([['6', 'H'], ['7', 'D'], ['8', 'H'], ['9', 'H'], ['T', 'H']]))
+        self.assertEqual(['Flush', 'J', 'T', '8', '7', '6'],
+                         poker_hand([['6', 'H'], ['7', 'H'], ['8', 'H'], ['T', 'H'], ['J', 'H']]))
+        self.assertEqual(['Full House', '7'], poker_hand([['6', 'H'], ['6', 'D'], ['7', 'S'], ['7', 'H'], ['7', 'C']]))
+        self.assertEqual(['Four of a kind', '7'],
+                         poker_hand([['6', 'H'], ['7', 'D'], ['7', 'S'], ['7', 'H'], ['7', 'C']]))
+        self.assertEqual(['Straight flush', 'T'],
+                         poker_hand([['6', 'H'], ['7', 'H'], ['8', 'H'], ['9', 'H'], ['T', 'H']]))
+
+    def test_who_win(self):
+        self.assertEqual('White wins', who_win('Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH'))
+        self.assertEqual('Black wins', who_win('Black: 2H 4S 4C 2D 4H White: 2S 8S AS QS 3S'))
+        self.assertEqual('Black wins', who_win('Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C KH'))
+        self.assertEqual('Tie', who_win('Black: 2H 3D 5S 9C KD White: 2D 3H 5C 9S KH'))
 
 if __name__ == '__main__':
     unittest.main()
