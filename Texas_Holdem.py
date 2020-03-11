6b62ad1bb35d33229cf4543a83bdc713fbc90802
jinchangxfd <1660857877@qq.com>
2020-03-12 00:12:35 +0800

    finish

--- Texas_Holdem.py
+++ Texas_Holdem.py
@@ -10,7 +10,6 @@ def cmp(a, b):
 # 判断牌型
 def poker_hand(hand_list=[]):
     re_list = sorted(hand_list, key=cmp_to_key(cmp))
-    print(re_list)
 
     # 同花顺
     if re_list[0][1] == re_list[1][1] == re_list[2][1] == re_list[3][1] == re_list[4][1] \
@@ -75,27 +74,185 @@ def cmp_poker(a, b):
                'Four of a kind', 'Straight flush']
     if lt_list.index(a[0]) > lt_list.index(b[0]):
         return 1
-    if lt_list.index(a[0]) > lt_list.index(b[0]):
-        return 0
+    if lt_list.index(a[0]) < lt_list.index(b[0]):
+        return -1
+
+    # 散牌
+    if a[0] == 'High Card':
+        if cmp(a[1], b[1]) > 0:
+            return 1
+        elif cmp(a[1], b[1]) < 0:
+            return -1
+        else:
+            if cmp(a[2], b[2]) > 0:
+                return 1
+            elif cmp(a[2], b[2]) < 0:
+                return -1
+            else:
+                if cmp(a[3], b[3]) > 0:
+                    return 1
+                elif cmp(a[3], b[3]) < 0:
+                    return -1
+                else:
+                    if cmp(a[4], b[4]) > 0:
+                        return 1
+                    elif cmp(a[4], b[4]) < 0:
+                        return -1
+                    else:
+                        if cmp(a[5], b[5]) > 0:
+                            return 1
+                        elif cmp(a[5], b[5]) < 0:
+                            return -1
+                        else:
+                            return 0
+
+    # 对子
+    if a[0] == 'Pair':
+        if cmp(a[1], b[1]) > 0:
+            return 1
+        elif cmp(a[1], b[1]) < 0:
+            return -1
+        else:
+            if cmp(a[2], b[2]) > 0:
+                return 1
+            elif cmp(a[2], b[2]) < 0:
+                return -1
+            else:
+                if cmp(a[3], b[3]) > 0:
+                    return 1
+                elif cmp(a[3], b[3]) < 0:
+                    return -1
+                else:
+                    if cmp(a[4], b[4]) > 0:
+                        return 1
+                    elif cmp(a[4], b[4]) < 0:
+                        return -1
+                    else:
+                        return 0
+
+    # 两对
+    if a[0] == 'Two Pairs':
+        if cmp(a[1], b[1]) > 0:
+            return 1
+        elif cmp(a[1], b[1]) < 0:
+            return -1
+        else:
+            if cmp(a[2], b[2]) > 0:
+                return 1
+            elif cmp(a[2], b[2]) < 0:
+                return -1
+            else:
+                if cmp(a[3], b[3]) > 0:
+                    return 1
+                elif cmp(a[3], b[3]) < 0:
+                    return -1
+                else:
+                    return 0
+
+    # 三条
+    if a[0] == 'Three of a Kind':
+        if cmp(a[1], b[1]) > 0:
+            return 1
+        elif cmp(a[1], b[1]) < 0:
+            return -1
+        else:
+            return 0
+
+    # 顺子
+    if a[0] == 'Straight':
+        if cmp(a[1], b[1]) > 0:
+            return 1
+        elif cmp(a[1], b[1]) < 0:
+            return -1
+        else:
+            return 0
+
+    # 同花
+    if a[0] == 'Flush':
+        if cmp(a[1], b[1]) > 0:
+            return 1
+        elif cmp(a[1], b[1]) < 0:
+            return -1
+        else:
+            if cmp(a[2], b[2]) > 0:
+                return 1
+            elif cmp(a[2], b[2]) < 0:
+                return -1
+            else:
+                if cmp(a[3], b[3]) > 0:
+                    return 1
+                elif cmp(a[3], b[3]) < 0:
+                    return -1
+                else:
+                    if cmp(a[4], b[4]) > 0:
+                        return 1
+                    elif cmp(a[4], b[4]) < 0:
+                        return -1
+                    else:
+                        if cmp(a[5], b[5]) > 0:
+                            return 1
+                        elif cmp(a[5], b[5]) < 0:
+                            return -1
+                        else:
+                            return 0
+
+    # 葫芦
+    if a[0] == 'Full House':
+        if cmp(a[1], b[1]) > 0:
+            return 1
+        elif cmp(a[1], b[1]) < 0:
+            return -1
+        else:
+            return 0
+
+    # 铁支
+    if a[0] == 'Four of a kind':
+        if cmp(a[1], b[1]) > 0:
+            return 1
+        elif cmp(a[1], b[1]) < 0:
+            return -1
+        else:
+            return 0
+
+    # 同花顺
+    if a[0] == 'Straight flush':
+        if cmp(a[1], b[1]) > 0:
+            return 1
+        elif cmp(a[1], b[1]) < 0:
+            return -1
+        else:
+            return 0
 
 
 # 判断胜负
 def who_win(input_str):
     # 数据规范化
     input_list = input_str.split(' ')
-    print(input_list)
     name1 = input_list[0][:-1]
     hand1 = []
     hand2 = []
     for i in range(1, 6):
         hand1.append(list(input_list[i]))
-    print(hand1)
     poker_hand(hand1)
     name2 = input_list[6][:-1]
     for i in range(7, 12):
         hand2.append(list(input_list[i]))
-    print(hand2)
+
+    hand1 = poker_hand(hand1)
+    hand2 = poker_hand(hand2)
+
+    x = cmp_poker(hand1, hand2)
+    if x == 1:
+        print(name1 + ' wins')
+        return name1 + ' wins'
+    elif x == -1:
+        print(name2 + ' wins')
+        return name2 + ' wins'
+    else:
+        print('Tie')
+        return 'Tie'
 
 
 if __name__ == '__main__':
     who_win('Black: AH KD 7S 4H 7C White: 2C 3H 4S 8C AH')
+    who_win('Black: 2H 3D 5S 9C KD B: 2C 3H 4S 8C AH')
